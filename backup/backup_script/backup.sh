60fa5a0ad71be632219144da53902533952907b4
naruto4999 <kk.singh4999@gmail.com>
2024-01-04 05:06:55 +0530

    Modify dockerfile to manage database backups.

--- backup/backup_script/backup.sh
+++ backup/backup_script/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define backup filename with timestamp
-BACKUP_FILE="/backup/db_backup_$(date +%Y-%m-%d_%H-%M-%S).sql"
+BACKUP_FILE="/database_backups/db_backup_$(date +%Y-%m-%d_%H-%M-%S).sql"
 
 # Run pg_dump command to backup the database
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U $POSTGRES_USER -h $DB_HOST $POSTGRES_DB > $BACKUP_FILE
--- backup/start_backup.sh
+++ backup/start_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep $((($(date -d '21:30' +%s)-$(date +%s)+86400) % 86400))
+sleep $((($(date -d '23:41' +%s)-$(date +%s)+86400) % 86400))
 /backup/backup_script/backup.sh
