53fe396ba0d4899d2f8b971f8f6359530e341727
naruto4999 <kk.singh4999@gmail.com>
2024-01-04 06:17:06 +0530

    Modify start_backup.sh to backup the database daily.

--- backup/start_backup.sh
+++ backup/start_backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 chown root:root /backup/backup_script/backup.sh
 
-sleep $((($(date -d '00:41' +%s)-$(date +%s)+86400) % 86400))
-/backup/backup_script/backup.sh
+
+while true
+do
+    sleep $((($(date -d '00:51' +%s)-$(date +%s)+86400) % 86400))
+    /backup/backup_script/backup.sh
+done
