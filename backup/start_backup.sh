25503d54fae6511a5df8c2a5a69a4943f9f5e09a
naruto4999 <kk.singh4999@gmail.com>
2024-01-04 06:29:06 +0530

    Modify the time of daily backups.

--- backup/start_backup.sh
+++ backup/start_backup.sh
@@ -4,6 +4,6 @@ chown root:root /backup/backup_script/backup.sh
 
 while true
 do
-    sleep $((($(date -d '00:51' +%s)-$(date +%s)+86400) % 86400))
+    sleep $((($(date -d '21:30' +%s)-$(date +%s)+86400) % 86400))
     /backup/backup_script/backup.sh
 done
